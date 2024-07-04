405c82fd3af8c678a8404d88070762f3a3edc3e2
Webinar <webinatboot@163.com>
2024-07-04 15:58:46 +0800

    重置架构

--- webinar-boot-java/webinar-boot-common/src/main/java/com/webinar/dao/SysUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.webinar.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.webinar.entity.SysUser;
-
-public interface SysUserMapper extends BaseMapper<SysUser> {
-
-
-}
--- webinar-boot-java/webinar-boot-common/src/main/java/com/webinar/dao/SysUserPermissionRelationMapper.java
+++
@@ -1,8 +0,0 @@
-package com.webinar.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.webinar.entity.SysUserPermissionRelation;
-
-public interface SysUserPermissionRelationMapper extends BaseMapper<SysUserPermissionRelation> {
-
-}
